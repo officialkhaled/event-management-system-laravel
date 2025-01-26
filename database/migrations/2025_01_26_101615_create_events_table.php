5c3db5e76c85f3131332dc57a53128da74433ae8
Khaled <khaled@skylarksoft.com>
2025-01-26 17:50:29 +0600

    feature: events feature add

---
+++ database/migrations/2025_01_26_101615_create_events_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title');
+            $table->unsignedBigInteger('division_id');
+            $table->unsignedBigInteger('district_id')->nullable();
+            $table->unsignedBigInteger('upazila_id')->nullable();
+            $table->unsignedBigInteger('union_id')->nullable();
+            $table->date('date');
+            $table->string('from_time')->nullable();
+            $table->string('to_time')->nullable();
+            $table->text('description')->nullable();
+            $table->tinyInteger('status')->comment('0: Inactive, 1: Active')->default(1);
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
